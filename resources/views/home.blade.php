279b6a09d45e40afb28bdf9371faa36d453c2ff3
HASHIR MERAJ <hashirmeraj1@gmail.com>
2024-10-17 01:30:42 -0700

    updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
-@foreach ($users as $id => $user )
-    <h1>Name: {{$user['name']}} | Phone: {{$user['phone']}}
-        <a href="{{route('view.user',$id)}}"> Show</a>
+@foreach ($users as $id => $user)
+    <h1>Name: {{ $user['name'] }} | Phone: {{ $user['phone'] }}
+        <a href="{{ route('view.user', $id) }}"> Show</a>
     </h1>
+    <h1>{{ $user['name'] }}</h1>
 @endforeach
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-function getUser(){
+function getUser()
+{
     return [
         1 => ['name' => 'Hashir Meraj', 'phone' => '03202262029', 'city' => 'Karachi'],
         2 => ['name' => 'Ali Ahmed', 'phone' => '03332223344', 'city' => 'Lahore'],
         3 => ['name' => 'Sara Khan', 'phone' => '03112234567', 'city' => 'Islamabad'],
-        4 => ['name' => 'Ayesha Ali', 'phone' => '03001234567', 'city' => 'Peshawar']
+        4 => ['name' => 'Ayesha Ali', 'phone' => '03001234567', 'city' => 'Peshawar'],
     ];
 }
 
@@ -15,13 +16,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function(){
+Route::get('/home', function () {
     $users = getUser();
+
     return view('home', ['users' => $users]);
 });
 Route::get('users/{id}', function ($id) {
     $users = getUser();
     abort_if(!isset($users[$id]), 404);
+
     $user = $users[$id];
+
     return view('users', ['id' => $user]);
 })->name('view.user');
