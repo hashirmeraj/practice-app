b97520ef934ff690b6b669aaf8758d77b6f2bcb1
HASHIR MERAJ <hashirmeraj1@gmail.com>
2024-11-03 01:06:37 -0800

    Update adduser.blade.php

--- resources/views/adduser.blade.php
+++ resources/views/adduser.blade.php
@@ -61,7 +61,7 @@
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="City">
             </div>
-
+            <button type="submit" class="p-3 bg-slate-800 rounded-lg">Submit</button>
         </form>
 
     </div>
