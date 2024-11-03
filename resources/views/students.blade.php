3e92ddd7fe878f1a1b1d017c3a43237a0c43e62c
HASHIR MERAJ <hashirmeraj1@gmail.com>
2024-11-03 00:30:54 -0700

    Update students.blade.php

--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -41,71 +41,22 @@
       
       <tbody>
           <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+              @foreach ($data as $id => $users)
+                  
               
               <td class="px-6 py-4">
-                  Silver
+                {{$users->id }}
               </td>
               <td class="px-6 py-4">
-                  Laptop
+                {{$users->Name }}
               </td>
               <td class="px-6 py-4">
-                  $2999
+                {{$users->Email }}
               </td>
-              <td class="px-6 py-4">
-                  <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-              </td>
+              @endforeach
+              
           </tr>
-          <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                  Microsoft Surface Pro
-              </th>
-              <td class="px-6 py-4">
-                  White
-              </td>
-              <td class="px-6 py-4">
-                  Laptop PC
-              </td>
-              <td class="px-6 py-4">
-                  $1999
-              </td>
-              <td class="px-6 py-4">
-                  <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-              </td>
-          </tr>
-          <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                  Magic Mouse 2
-              </th>
-              <td class="px-6 py-4">
-                  Black
-              </td>
-              <td class="px-6 py-4">
-                  Accessories
-              </td>
-              <td class="px-6 py-4">
-                  $99
-              </td>
-              <td class="px-6 py-4">
-                  <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-              </td>
-          </tr>
-          <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                  Google Pixel Phone
-              </th>
-              <td class="px-6 py-4">
-                  Gray
-              </td>
-              <td class="px-6 py-4">
-                  Phone
-              </td>
-              <td class="px-6 py-4">
-                  $799
-              </td>
-              <td class="px-6 py-4">
-                  <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-              </td>
-          </tr>
+         
           
       </tbody>
   </table>
